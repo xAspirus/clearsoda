bc670c05bc22dc6b2243511f5f08fdbfa27b21c5
xAspirus <movxa@protonmail.com>
2022-06-24 18:26:27 +0530

    Fix project.py

--- examples/demonstration/main.py
+++ examples/demonstration/main.py
@@ -1,7 +1,18 @@
 from clearsoda import *
 
-sprite = Sprite(name=__name__, costumes=["assets/scratchcat.svg"]).Code(
-    WhenFlagClicked(
-        Say("Hello, World!"),
-    )
+sprite = Sprite(name=__name__, costumes=["assets/scratchcat.svg"]).WhenFlagClicked(
+    *flatten(
+        [
+            [
+                SayFor(f"{i} bottles of beer on the wall, {i} bottles of beer.", 0.5),
+                SayFor(
+                    f"Take one down and pass it around, {i-1} bottles of beer on the wall.",
+                    0.5,
+                ),
+            ]
+            for i in range(99, 1, -1)
+        ]
+    ),
+    SayFor("1 bottle of beer on the wall, 1 bottle of beer.", 0.5),
+    SayFor("Take one down and pass it around, No bottles of beer on the wall.", 0.5),
 )
